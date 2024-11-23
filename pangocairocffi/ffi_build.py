4b7c09ab79fcd378b7dd0e99d410fac35569bddc
George Harker <george@george-graphics.co.uk>
2024-11-23 09:17:56 -0800

    api mode

--- pangocairocffi/ffi_build.py
+++ pangocairocffi/ffi_build.py
@@ -5,10 +5,12 @@
     Build the cffi bindings
 """
 
+import os
 import sys
 from pathlib import Path
+
 from cffi import FFI
-from pangocffi import ffi as ffi_pango
+
 
 sys.path.append(str(Path(__file__).parent))
 
@@ -29,10 +31,50 @@ c_definitions_pangocairo = c_definitions_pangocairo_file.read()
 
 # cffi definitions, in the order outlined in:
 ffi = FFI()
-ffi.include(ffi_pango)
-ffi.set_source('pangocairocffi._generated.ffi', None)
-ffi.cdef(c_definitions_cairo)
+
+if ('PANGOCFFI_API_MODE' in os.environ and
+        int(os.environ['PANGOCFFI_API_MODE']) == 1):
+    from pangocffi.ffi_build import ffi as ffi_pango
+    ffi.include(ffi_pango)
+else:
+    from pangocffi import ffi as ffi_pango
+    ffi.include(ffi_pango)
+
+if ('CAIROCFFI_API_MODE' in os.environ and
+        int(os.environ['CAIROCFFI_API_MODE']) == 1):
+    from cairocffi.ffi import ffi as ffi_cairo
+    ffi.include(ffi_cairo)
+else:
+    ffi.cdef(c_definitions_cairo)
+
 ffi.cdef(c_definitions_pangocairo)
+
+if ('PANGOCAIROCFFI_API_MODE' in os.environ and
+        int(os.environ['PANGOCAIROCFFI_API_MODE']) == 1):
+    ffi.set_source_pkgconfig(
+        '_pangocairocffi',
+        ['pangocairo', 'pango', 'glib-2.0'],
+        """
+        #include "glib.h"
+        #include "glib-object.h"
+        #include "pango/pango.h"
+        #include "pango/pangocairo.h"
+        #include "cairo-pdf.h"
+        #include "cairo-svg.h"
+        #include "cairo-ps.h"
+        #include "cairo-quartz.h"
+
+        #include "xcb/xproto.h"
+        #include "xcb/xcb.h"
+        #include "xcb/xcbext.h"
+        #include "xcb/render.h"
+        #include "cairo-xcb.h"
+        """,
+        sources=[]
+    )
+
+else:
+    ffi.set_source('pangocairocffi._generated.ffi', None)
 
 if __name__ == '__main__':
     ffi.compile()
--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+import os
 import sys
+
 from setuptools import setup
+
 
 if sys.version_info.major < 3:
     raise RuntimeError(
         'pangocairocffi does not support Python 2.x. Please use Python 3.'
     )
 
-setup()
+if ('PANGOCAIROCFFI_API_MODE' in os.environ and
+        int(os.environ['PANGOCAIROCFFI_API_MODE']) == 1):
+    setup(
+        install_requires=['cffi >= 1.1.0'],
+        setup_requires=['setuptools_scm', 'cffi >= 1.1.0'],
+        cffi_modules=['pangocairocffi/ffi_build.py:ffi'],
+        packages=['pangocairocffi']
+    )
+else:
+    setup()
