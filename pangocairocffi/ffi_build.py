ec9ce322a2d1a4dfe992abad2a77019fba5be832
George Harker <george@george-graphics.co.uk>
2024-12-09 12:32:45 -0800

    automatically determine which mode to build in

--- pangocairocffi/ffi_build.py
+++ pangocairocffi/ffi_build.py
@@ -5,124 +5,85 @@
     Build the cffi bindings
 """
 
-import os
 import sys
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 from pathlib import Path
 from warnings import warn
 
 
 from cffi import FFI
+from cffi.error import VerificationError
+
 
 sys.path.append(str(Path(__file__).parent))
 
 
-api_mode = False
-if ('PANGOCAIROCFFI_API_MODE' in os.environ and
-        int(os.environ['PANGOCAIROCFFI_API_MODE']) == 1):
-    # Allow explicit disable of api_mode
-    api_mode = True
-
-# Create an empty _generated folder if needed
-if not api_mode:
-    (Path(__file__).parent / '_generated').mkdir(exist_ok=True)
-
-# Read the CFFI definitions
-c_definitions_cairo_file = open(
-    str(Path(__file__).parent / 'c_definitions_cairo.txt'),
-    'r'
-)
-c_definitions_cairo = c_definitions_cairo_file.read()
-c_definitions_pangocairo_file = open(
-    str(Path(__file__).parent / 'c_definitions_pangocairo.txt'),
-    'r'
-)
-c_definitions_pangocairo = c_definitions_pangocairo_file.read()
-
-# cffi definitions, in the order outlined in:
-ffi = FFI()
-
-xcffib_available = False
-try:
-    from xcffib.ffi_build import ffi as xcb_ffi
-except ImportError:
-    warn("xcfiib not available")
-else:
-    xcffib_available = True
-
-pangocffi_api_mode = False
-try:
-    from pangocffi import ffi_build as ffi_pango
-    ffi.include(ffi_pango.ffi)
-    pangocffi_api_mode = True
-except ImportError:
-    from pangocffi import ffi as ffi_pango
-    ffi.include(ffi_pango)
-    if api_mode:
-        warn("pangocffi is not available in API mode, disabling API mode")
-        api_mode = False
-
-cairocffi_api_mode = False
-try:
-    from cairocffi.ffi_build import ffi as ffi_cairo
-    ffi.include(ffi_cairo)
-    cairocffi_api_mode = True
-except ImportError:
-    ffi.cdef(c_definitions_cairo)
-    if api_mode:
-        warn("cairocffi is not available in API mode, disabling API mode")
-        api_mode = False
-
-
-ffi.cdef(c_definitions_pangocairo)
-
-if api_mode:
-    ffi.set_source_pkgconfig(
-        '_pangocairocffi',
-        ['pangocairo', 'pango', 'glib-2.0'],
-        r"""
-        #include "glib.h"
-        #include "glib-object.h"
-        #include "pango/pango.h"
-        #include "pango/pangocairo.h"
-        #include "cairo-pdf.h"
-        #include "cairo-svg.h"
-        #include "cairo-ps.h"
-        #if defined(_WIN64) || defined(_WIN32)
-        #include "cairo-win32.h"
-        #endif
-        """ +
-        (r"""
-        #include "xcb/xcb.h"
-        #include "xcb/xproto.h"
-        #include "xcb/xcbext.h"
-        #include "xcb/render.h"
-        #include "cairo-xcb.h"
-        """ if xcffib_available else "") +
-        r"""
-        /* Deal with some newer definitions for compatibility */
-        #if CAIRO_VERSION < 11702
-        #define CAIRO_FORMAT_RGBA128F 7
-        #define CAIRO_FORMAT_RGB96F 6
-        #endif
-        #if CAIRO_VERSION < 11800
-        #include <stdio.h>
-        #include <stdbool.h>
-        void cairo_set_hairline(cairo_t*, cairo_bool_t);
-        cairo_bool_t cairo_get_hairline(cairo_t*);
-        void cairo_set_hairline(cairo_t*, cairo_bool_t) {
-            fprintf(stderr, "Unimplemented!!\n");
-        }
-        cairo_bool_t cairo_get_hairline(cairo_t*) {
-            fprintf(stderr, "Unimplemented!!\n");
-            return false;
-        }
-        #endif
-        """,
-        sources=[]
+def ffi_for_mode(mode):
+    # Read the CFFI definitions
+    c_definitions_cairo_file = open(
+        str(Path(__file__).parent / 'c_definitions_cairo.txt'),
+        'r'
     )
+    c_definitions_cairo = c_definitions_cairo_file.read()
+    c_definitions_pangocairo_file = open(
+        str(Path(__file__).parent / 'c_definitions_pangocairo.txt'),
+        'r'
+    )
+    c_definitions_pangocairo = c_definitions_pangocairo_file.read()
 
-else:
-    ffi.set_source('pangocairocffi._generated.ffi', None)
+    # cffi definitions, in the order outlined in:
+    ffi = FFI()
+
+    from pangocffi import cffi_mode as pango_cffi_mode
+    from pangocffi.ffi_build import ffi_for_mode as pango_ffi_for_mode
+    pango_ffi = pango_ffi_for_mode(mode)
+    ffi.include(pango_ffi)
+
+    from cairocffi import cffi_mode as cairo_cffi_mode
+    from cairocffi.ffi_build import ffi_for_mode as cairo_ffi_for_mode
+    from cairocffi.ffi_build import c_source_cairo, c_source_cairo_compat
+    cairo_ffi = cairo_ffi_for_mode(mode)
+    ffi.include(cairo_ffi)
+
+    ffi.cdef(c_definitions_pangocairo)
+
+    if mode == "api":
+        ffi.set_source_pkgconfig(
+            '_pangocairocffi',
+            ['pangocairo', 'pango', 'glib-2.0'],
+            c_source_cairo +
+            r"""
+            #include "glib.h"
+            #include "glib-object.h"
+            #include "pango/pango.h"
+            #include "pango/pangocairo.h"
+            """ + c_source_cairo_compat,
+            sources=[]
+        )
+
+    else:
+        ffi.set_source('_pangocairocffi', None)
+    return ffi
+
+
+def build_ffi():
+    """
+    This will be called from setup() to return an FFI
+    which it will compile - work out here which type is
+    possible and return it.
+    """
+    try:
+        ffi_api = ffi_for_mode("api")
+        ffi_api.compile(verbose=True)
+        return ffi_api
+    except (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+            VerificationError) as e:
+        warn("Falling back to precompiled python mode: {}".format(str(e)))
+
+        ffi_abi = ffi_for_mode("abi")
+        ffi_abi.compile(verbose=True)
+        return ffi_abi
+
 
 if __name__ == '__main__':
-    ffi.compile()
+    build_ffi()
--- setup.py
+++ setup.py
@@ -9,15 +9,11 @@ if sys.version_info.major < 3:
         'pangocairocffi does not support Python 2.x. Please use Python 3.'
     )
 
-if ('PANGOCAIROCFFI_API_MODE' in os.environ and
-        int(os.environ['PANGOCAIROCFFI_API_MODE']) == 1):
-    setup(
-        name='pangocairocffi',
-        # when cairocffi, pangocffi are updated, bump to include API mode
-        install_requires=['cffi >= 1.1.0', 'cairocffi >= 1.7.1', 'pangocffi >= 0.13.0'],
-        setup_requires=['cffi >= 1.1.0', 'cairocffi >= 1.7.1', 'pangocffi >= 0.13.0'],
-        cffi_modules=['pangocairocffi/ffi_build.py:ffi'],
-        packages=['pangocairocffi']
-    )
-else:
-    setup()
+setup(
+    name='pangocairocffi',
+    # when cairocffi, pangocffi are updated, bump to include API mode
+    install_requires=['cffi >= 1.1.0', 'cairocffi >= 1.7.1', 'pangocffi >= 0.13.0'],
+    setup_requires=['cffi >= 1.1.0', 'cairocffi >= 1.7.1', 'pangocffi >= 0.13.0'],
+    cffi_modules=['pangocairocffi/ffi_build.py:build_ffi'],
+    packages=['pangocairocffi']
+)
