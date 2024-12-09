486a316b701496283f7ebe3f2d7cb210492e3a9e
George Harker <george@george-graphics.co.uk>
2024-12-09 13:28:02 -0800

    fix setuptools import

--- pangocairocffi/ffi_build.py
+++ pangocairocffi/ffi_build.py
@@ -6,7 +6,7 @@
 """
 
 import sys
-from setuputils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+from setuptools.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 from pathlib import Path
 from warnings import warn
 
